8f39675f1955bc02679307d941f36100fd0537a8
maximiliem <maxibertta@gmail.com>
2026-01-20 23:02:16 -0300

    ejercicio contador numeros positivos con while hecho

---
+++ 05-bucles/contadorPositivosWhile.pas
@@ -0,0 +1,23 @@
+program ContadorPositivos;
+
+uses 
+    crt;
+
+var
+    numero, contador, suma : integer;
+
+begin
+    contador := 0;
+    suma := 0;
+    write('Ingresa un número (0 o negativo para terminar): ');
+            readln(numero);
+    while (numero > 0) do
+        begin
+            contador := contador + 1;
+            suma := numero + suma;
+            write('Ingresa un número (0 o negativo para terminar): ');
+            readln(numero);
+        end;
+    writeln('Ingresaste ', contador, ' números positivos.');
+    writeln('La suma total es: ', suma)
+end.
